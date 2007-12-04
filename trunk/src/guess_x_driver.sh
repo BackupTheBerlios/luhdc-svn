5a7ef540fea57fc1226b7539ad139c7fb9803a0a
neuralgya <neuralgya@ecfc3df3-2321-0410-b6f9-c94758ed7223>
2007-12-04 19:10:23 +0000

    * Revised advisor * Added autostart link * Updates guessing X driver

--- trunk/src/guess_x_driver.sh
+++ trunk/src/guess_x_driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-DRV=$(echo $(grep -i -A 30 "Section.\+\"Device\"" /etc/X11/xorg.conf|grep -i -m1 Driver|sed s/"driver"/""/gi)|cut -f2 -d"\"");
+DRV=$(echo $(grep -i -A 30 "Section.\+\"Device\"" /etc/X11/xorg.conf|grep -i "Driver"|grep "\".\+\""|sed s/"driver"/""/gi)|cut -f2 -d"\"");
 GXDRIVER=$(echo $DRV|sed s/" \+"/""/g|sed s/"\""/""/g)
 MPATHS=$(grep module /etc/X11/xorg.conf|sed  s/"modulepath"/""/gi|sed s/"\""/""/g)
 MPATHS="/usr/lib/xorg/modules $MPATHS"
@@ -26,7 +26,7 @@ then
   if [ -z "$1" ]
   then
     PAK=$(dpkg -S $GXDLOC|cut -f1 -d":")
-    PAKVER=$(dpkg -l|grep $PAK|sed s/" \+"/" "/g|cut -f2,3 -d" "|sed s/" "/" version "/g)
+    PAKVER=$(dpkg -l $PAK|tail -1|sed s/" \+"/" "/g|cut -f2,3 -d" "|sed s/" "/" version "/g)
     echo "$GXDRIVER (Provided by) $PAKVER"
   else
     echo $GXDRIVER
